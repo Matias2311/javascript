d9d970e7648b194b0e0a43026ec58d9fdcbaa6d1
Matias2311 <136719502+Matias2311@users.noreply.github.com>
2023-12-06 15:33:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+// Se declaran los números y la operación mediante prompt
+
+let num1 = parseInt (prompt ("Ingresar primer numero"));
+let num2 = parseInt (prompt ("Ingresar segundo numero"));
+let operacion = prompt ("Ingresar operacion que se quiere realizar mediante simbolos (*, +, -, /)");
+
+// Función para sumar dos números
+function sumar(a, b) {
+    return a + b;
+  }
+  
+  // Función para restar dos números
+  function restar(a, b) {
+    return a - b;
+  }
+  
+  // Función para multiplicar dos números
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  // Función para dividir dos números
+  function dividir(a, b) {
+    if (b !== 0) {
+      return a / b;
+    } else {
+      return "Error: No es posible dividir por cero.";
+    }
+  }
+  
+  // Función principal de la calculadora
+  function calculadora(operacion, a, b) {
+    switch (operacion) {
+      case "+":
+        return sumar(a, b);
+      case "-":
+        return restar(a, b);
+      case "*":
+        return multiplicar(a, b);
+      case "/":
+        return dividir(a, b);
+      default:
+        return "Error";
+    }
+  }
+  
+  // Calculo de resultado
+  var resultado = calculadora(operacion, num1, num2);
+
+  // Mostrar mensaje 
+  alert ("Resultado: " + resultado);
+  
+  
+
